f6822d8cf08b94721c4fa8c9a6b8474275665243
Kevin Herrera <1herrerake@gmail.com>
2021-02-21 19:08:20 -0600

    Update database config

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,12 +1,3 @@
 module.exports = ({ env }) => ({
-  url: env("STRAPI_URL"),
-  port: env.int("PORT", 1337),
-  admin: {
-    auth: {
-      secret: env(
-        "ADMIN_JWT_SECRET",
-        "44hHlcgSC2GS89dIsPI/5xbbzOXQ9BFOh5dAeo9UbxdyfFMiqLgk/TNaWJavZQDtzsv3bIVTyiG+QA4VWAAuYQ"
-      ),
-    },
-  },
+  url: env("HEROKU_URL"),
 });
